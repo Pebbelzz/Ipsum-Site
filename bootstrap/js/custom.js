b73ab0fe67757c481e85c3da88f750db8d1ff134
Pebbelzz <greeneyes14j@gmail.com>
2016-01-17 18:26:58 -0500

    Added all files of site

---
+++ bootstrap/js/custom.js
@@ -0,0 +1,20 @@
+
+// Navagation menu for website, displayed on each page
+$(document).ready(function(){
+	$( "#main_nav" ).html( '<li><a class="btn btn-default" href="index.html">Home</a></li>'+
+			'<li><a class="btn btn-default" href="minion.html">Minion Ipsum</li>'+
+			'<li><a class="btn btn-default" href="hodor.html">Hodor Ipsum</li>'+
+			'<li><a class="btn btn-default" href="bacon.html">Bacon Ipsum</a></li>'+
+			'<li><a class="btn btn-default" href="doctor_who.html">Dr Who Ipsum</a></li>'+
+			'<li><a class="btn btn-default" href="tuna.html">Tuna Ipsum</a></li>'+
+			'<li><a class="btn btn-default" href="liqour.html">Liqour Ipsum</a></li>'+
+			'<li><a class="btn btn-default" href="cupcake.html">Cupcake Ipsum</a></li>'+
+			'<li><a class="btn btn-default" href="zombie.html">Zombie Ipsum</a></li>'+
+			'<li><a class="btn btn-default" href="seinfeld.html">Seinfeld Ipsum</a></li>'+
+			'<li><a class="btn btn-default" href="veggie.html">Veggie Ipsum</a></li> ');
+})
+
+$(document).ready(function(){
+	$( "header" ).html( '<h1>Fun Ipsums</h1>'+
+			'<div class="topholder"></div>');
+});
